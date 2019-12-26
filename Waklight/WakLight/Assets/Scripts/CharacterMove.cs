daf94ef4df7c5ef6d9e361f40ae10f5af074374e
Areum Yang <s2018s10@e-mirim.hs.kr>
2019-12-26 20:51:54 +0900

    ◾ 마우스따라 움직이는 거 완성

---
+++ Waklight/WakLight/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMove : MonoBehaviour
+{
+    Vector3 pos;
+    int size = 9;
+    Image m_Image;
+    public Sprite m_Sprite1;
+    public Sprite m_Sprite2;
+
+    void Start()
+    {
+        pos = this.gameObject.transform.position;
+        Debug.Log(pos);
+        m_Image = GetComponent<Image>();
+    }
+    
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_Image.sprite = m_Sprite1;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_Image.sprite = m_Sprite2;
+        }
+
+        else
+        {
+            Debug.Log("나 마우스 아무것도 안하는 중");
+            if (pos.x > mousePos.x)
+            {
+                transform.Translate(-size, 0, 0);
+            }
+            if (pos.x < mousePos.x)
+            {
+                transform.Translate(size, 0, 0);
+            }
+        }
+    }
+}
---
+++ Waklight/WakLight/Assets/Scripts/PersonClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonClick : MonoBehaviour
+{
+    float MaxDistance = 15f;
+    Vector3 MousePosition;
+    Camera Camera;
+
+    void Start()
+    {
+        Camera = GetComponent<Camera>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("클릭");
+            MousePosition = Input.mousePosition;
+            MousePosition = Camera.ScreenToWorldPoint(MousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(MousePosition, transform.forward, MaxDistance);
+            Debug.DrawRay(MousePosition, transform.forward * 10, Color.red, 0.3f);
+            if (hit)
+            {
+                hit.transform.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+        }
+    }
+}
